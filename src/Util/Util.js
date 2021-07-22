b409c2e0ca6955c7cd34253b1370b0e304f4ea3c
Vrushank Patel <vrushankpatel5@gmail.com>
2021-07-22 16:02:31 +0530

    changed identifier

--- src/Util/Util.js
+++ src/Util/Util.js
@@ -3,12 +3,15 @@ const encodedIdentifier = "aHR0cHM6Ly9pYm9hcmR4Lmhlcm9rdWFwcC5jb20vYXBp";
 
 // Dev
 // const encodedIdentifier = "aHR0cDovL2xvY2FsaG9zdDo1MDAwL2FwaQ=="
+
+const socketEndPoint = "https://iboardx-streamer.herokuapp.com/";
 var axios = require('axios');
 const util = {
     identifier: atob(encodedIdentifier),
-    socketEndpoint: "https://iboardx-streamer.herokuapp.com/",
+    socketEndpoint: socketEndPoint,
     awakeEndpoint: () => {
         axios.get(atob(encodedIdentifier));
+        axios.get(socketEndPoint);
     }
 };
 
