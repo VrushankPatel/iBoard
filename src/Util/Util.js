1526dcc0a131bd51cbec43805ad69610dadc94ae
Vrushank Patel <vrushankpatel5@gmail.com>
2021-10-29 14:01:12 +0530

    removed identifier

--- src/Util/Util.js
+++ src/Util/Util.js
@@ -1,25 +1,16 @@
 // PROD
 const encodedIdentifier1 = "aHR0cHM6Ly9pYm9hcmR4Lmhlcm9rdWFwcC5jb20vYXBp";
-const encodedIdentifier2 = "aHR0cHM6Ly9pYm9hcmQtc2VydmVyMi5oZXJva3VhcHAuY29tL2FwaQ=="
 // Dev
 // const encodedIdentifier = "aHR0cDovL2xvY2FsaG9zdDo1MDAwL2FwaQ=="
-
-const getUrlByGMTFn = () => {
-    const gmtHour = new Date().toUTCString().split(" ")[4].split(":")[0];
-    if (gmtHour >= 7 && gmtHour <= 19) {
-        return atob(encodedIdentifier1);
-    }
-    return atob(encodedIdentifier2);
-}
 
 const socketEndPoint = "https://iboardx-streamer.herokuapp.com/";
 
 var axios = require('axios');
 const util = {
-    identifier: getUrlByGMTFn(),
+    identifier: atob(encodedIdentifier1),
     socketEndpoint: socketEndPoint,
     awakeEndpoint: () => {
-        axios.get(getUrlByGMTFn());
+        axios.get(atob(encodedIdentifier1));
         axios.get(socketEndPoint);
     }
 };
