b4ebcf1b2d77fde4b1ad3fa36c6a68cc3a18cafb
Vrushank Patel <vrushankpatel5@gmail.com>
2021-12-22 17:31:19 +0530

    added env params

--- src/Util/Util.js
+++ src/Util/Util.js
@@ -1,16 +1,17 @@
-// PROD
-const encodedIdentifier1 = "aHR0cHM6Ly9pYm9hcmR4Lmhlcm9rdWFwcC5jb20vYXBp";
 // Dev
 // const encodedIdentifier = "aHR0cDovL2xvY2FsaG9zdDo1MDAwL2FwaQ=="
+// const socketEndPoint = "http://localhost:4001/";
 
+// PROD
+const encodedIdentifier = "aHR0cHM6Ly9pYm9hcmR4Lmhlcm9rdWFwcC5jb20vYXBp";
 const socketEndPoint = "https://iboardx-streamer.herokuapp.com/";
 
 var axios = require('axios');
 const util = {
-    identifier: atob(encodedIdentifier1),
+    identifier: atob(encodedIdentifier),
     socketEndpoint: socketEndPoint,
     awakeEndpoint: () => {
-        axios.get(atob(encodedIdentifier1));
+        axios.get(atob(encodedIdentifier));
         axios.get(socketEndPoint);
     }
 };
