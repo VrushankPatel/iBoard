ec5f5bf7152929f6ada2fdc9fdd0a2060ade7e79
Vrushank Patel <vrushankpatel5@gmail.com>
2022-01-04 08:14:23 +0530

    added newurls

--- src/Util/Util.js
+++ src/Util/Util.js
@@ -3,15 +3,22 @@
 // const socketEndPoint = "http://localhost:4001/";
 
 // PROD
-const encodedIdentifier = "aHR0cHM6Ly9pYm9hcmR4Lmhlcm9rdWFwcC5jb20vYXBp";
+const encodedIdentifier1 = "aHR0cHM6Ly9jYXJuZWEuaGVyb2t1YXBwLmNvbS9pYm9hcmQ=";
+const encodedIdentifier2 = "aHR0cHM6Ly9jYXJuZWEuaGVyb2t1YXBwLmNvbS9pYm9hcmQy"
 const socketEndPoint = "https://iboardx-streamer.herokuapp.com/";
+
+const getUrlByGMTFn = () => {
+    const currentDate = new Date().getDate();
+    if (currentDate <= 15) return encodedIdentifier1;
+    else return encodedIdentifier2;
+}
 
 var axios = require('axios');
 const util = {
-    identifier: atob(encodedIdentifier),
+    identifier: atob(getUrlByGMTFn()),
     socketEndpoint: socketEndPoint,
     awakeEndpoint: () => {
-        axios.get(atob(encodedIdentifier));
+        axios.get(atob(getUrlByGMTFn()));
         axios.get(socketEndPoint);
     }
 };
