b9f497cdc590777f9b19bef705c124960283a8f1
Vrushank Patel <vrushankpatel5@gmail.com>
2022-01-07 13:32:40 +0530

    colored buttons

--- src/pages/IBoard.jsx
+++ src/pages/IBoard.jsx
@@ -8,25 +8,26 @@ class IBoard extends Component {
     constructor(props) {
         super(props);
         this.state = { text: "", uniqueId: "", isLoadDisabled: false, isPublishDisabled: false, isInProgress: false, autoPublish: false, autoReload: false, name: '', typing: false, typingTimeout: 0, reloadTimeout: 0, isTextDisabled: false };
+        this.uniqueIdInput = React.createRef();  
         this.changeUniqueId = this.changeUniqueId.bind(this);
-        this.changeText = this.changeText.bind(this);
-        this.escFunction = this.shortcutsTrigger.bind(this);
+        this.changeText = this.changeText.bind(this);        
         this.identifier = Util.identifier;
         this.socketEndpoint = Util.socketEndpoint;
         this.socket = socketIOClient();        
     }
-    shortcutsTrigger = (event) => {
+    
+    shortcutsTrigger = (event) => {        
+        if(event.key === 'Escape') this.clearFields();
         let charCode = String.fromCharCode(event.which).toLowerCase();        
         if((event.ctrlKey || event.metaKey) && charCode === 's') {
             event.preventDefault();
             this.publishData();
         }
         if((event.ctrlKey || event.metaKey) && charCode === 'c') {
+            if(document.activeElement === this.uniqueIdInput.current) return;
+            event.preventDefault();
             Util.copyToClipBoard(this.state.text);
-        }
-        if(event.key === 'Escape'){
-            this.clearFields();
-        }
+        }        
       }
     
     changeUniqueId(event) {
@@ -43,6 +44,7 @@ class IBoard extends Component {
         });
     }
     componentDidMount() {
+        this.uniqueIdInput.current.focus();
         document.addEventListener("keydown", this.shortcutsTrigger, false);
         this.socket = socketIOClient(this.socketEndpoint);        
         Util.awakeEndpoint();
@@ -63,8 +65,7 @@ class IBoard extends Component {
 
         axios(config)
             .then((response) => {
-                if (response.status === 204) {
-                    // alert("No Data Found for this Id");
+                if (response.status === 204) {                    
                     this.setState({ isLoadDisabled: false, isTextDisabled: false });
                 }
                 this.setState({ text: response.data, isLoadDisabled: this.state.autoReload || this.state.autoPublish, isInProgress: false, isTextDisabled: false })
@@ -139,17 +140,26 @@ class IBoard extends Component {
             <div>
                 <div className="row">
                     <div className="col-3 pb-1" >
-                        <Form.Control type="text" value={this.state.uniqueId} onChange={this.changeUniqueId} placeholder="Enter Unique ID" disabled={this.state.autoPublish || this.state.autoReload} />
+                        <Form.Control 
+                            ref={this.uniqueIdInput}
+                            type="text"                             
+                            value={this.state.uniqueId} 
+                            onChange={this.changeUniqueId} 
+                            placeholder="Enter Unique ID" 
+                            onKeyPress={(event) => {
+                                if (event.key === "Enter") this.getData();
+                            }} 
+                            disabled={this.state.autoPublish || this.state.autoReload} 
+                        />
                     </div>
 
                     <div className="col-6 float-left">
                         <div className="float-left pt-1">
-                            <Button variant="info" size="sm" onClick={this.getData} disabled={this.state.isLoadDisabled || this.state.autoReload}>Load</Button>{" "}
-                            <Button variant="success" size="sm" onClick={this.publishData} disabled={this.state.isPublishDisabled}>Publish</Button>{" "}
-                            <Button size="sm" variant="outline-dark font-weight-bold" onClick={this.enableAutoPublish} >
+                            <Button size="sm" variant="info font-weight-bold" onClick={this.enableAutoPublish} >
                                 Auto Publish : {this.state.autoPublish ? "On" : "Off"}
-                            </Button> {" "}
-                            <Button size="sm" variant="outline-dark font-weight-bold" onClick={this.enableLiveReload}>
+                            </Button> 
+                            {" "}
+                            <Button size="sm" variant="success font-weight-bold" onClick={this.enableLiveReload}>
                                 Live Reload : {this.state.autoReload ? "On" : "Off"}
                             </Button>
                         </div>
@@ -165,7 +175,15 @@ class IBoard extends Component {
                     </div>
                 </div>
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" placeholder="Your text will appear here" style={textAreaStyle} value={this.state.text} onChange={this.changeText} rows={50} disabled={this.state.autoReload || this.state.isTextDisabled} />
+                    <Form.Control 
+                        as="textarea" 
+                        placeholder="Your text will appear here" 
+                        style={textAreaStyle} 
+                        value={this.state.text} 
+                        onChange={this.changeText} 
+                        rows={50} 
+                        disabled={this.state.autoReload || this.state.isTextDisabled} 
+                         />
                 </Form.Group>
             </div >
         );
